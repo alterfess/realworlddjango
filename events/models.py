b2152a19066b3cec6bccd6ba817490e6885027b5
George Sidorov <georgesidorov93@gmail.com>
2022-05-04 07:03:59 +0500

    Initial commit

---
+++ events/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200, default='', verbose_name='Название')
+    description = models.TextField(default='', verbose_name='Дата начала')
+    date_start = models.DateTimeField(verbose_name='Дата начала')
+    participants_number = models.PositiveSmallIntegerField(verbose_name='Количество участников')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Событие'
+        verbose_name_plural = 'События'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=90, default='', verbose_name='Категория')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
